94375f127163e76b769110ffd17fa16a1ae325db
KWYK <kwykyaw.99@gmail.com>
2024-02-01 00:20:46 +0800

    Fetch All Todo Items

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import "./reset.css";
-import "./App.css";
-import TodoForm from "./components/TodoForm.js";
-import TodoList from "./components/TodoList.js";
-import CheckAllAndRemaining from "./components/CheckAllAndRemaining.js";
-import TodoFilters from "./components/TodoFilters.js";
-import ClearCompletedBtn from "./components/ClearCompletedBtn.js";
+import './reset.css';
+import './App.css';
+import TodoForm from './components/TodoForm.js';
+import TodoList from './components/TodoList.js';
+import CheckAllAndRemaining from './components/CheckAllAndRemaining.js';
+import TodoFilters from './components/TodoFilters.js';
+import ClearCompletedBtn from './components/ClearCompletedBtn.js';
+import { useEffect, useState } from 'react';
 
 function App() {
+  let [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/todos')
+      .then((res) => res.json())
+      .then((todos) => setTodos(todos));
+  }, []);
+
   return (
-    <div className="todo-app-container">
-      <div className="todo-app">
+    <div className='todo-app-container'>
+      <div className='todo-app'>
         <h2>Todo App</h2>
         <TodoForm />
-        <TodoList />
+        <TodoList todos={todos} />
         <CheckAllAndRemaining />
-        <div className="other-buttons-container">
+        <div className='other-buttons-container'>
           <TodoFilters />
           <ClearCompletedBtn />
         </div>
